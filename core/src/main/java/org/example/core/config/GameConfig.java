2f131f8b16cb8abb19e685cfe414d90f39544b14
Rehan Manzoor <manzoorr@amazon.com>
2021-02-28 15:06:06 +0400

    load config from property file

--- core/src/main/java/org/example/core/config/GameConfig.java
+++ core/src/main/java/org/example/core/config/GameConfig.java
@@ -2,15 +2,20 @@ package org.example.core.config;
 
 import org.example.core.GuessCount;
 import org.example.core.MaxNumber;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
+@PropertySource("classpath:config/game.properties")
 public class GameConfig {
 
-    private int maxNumber = 25;
+    @Value("${game.maxNumber:20}")
+    private int maxNumber;
 
-    private int guessCount = 8;
+    @Value("${game.guessCount:5}")
+    private int guessCount;
 
     @Bean
     @MaxNumber
@@ -20,7 +25,7 @@ public class GameConfig {
 
     @Bean
     @GuessCount
-    public int getGuessCount(){
+    public int getGuessCount() {
         return this.guessCount;
     }
 }
